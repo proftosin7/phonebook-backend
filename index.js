7cbdda245db28ef8aea8d7547375c0ddd2441e1e
proftosin7 <proftosin7@gmail.com>
2025-02-06 00:29:42 +0100

    new file:   .gitignore 	new file:   dist/assets/index-B9Vlwi6d.js 	new file:   dist/assets/index-BUgtbOCU.js 	new file:   dist/assets/index-C2IUkbr9.css 	new file:   dist/index.html 	new file:   dist/vite.svg 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons =[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> 
+    <p>${date}</p>`);
+})
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+    if(person){
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+
+    const body = req.body;
+    if(!body.name || !body.number){
+        return res.status(400).json({
+            error: 'name or number is missing'
+        });
+    }
+
+    if(persons.find(person => person.name === body.name)){
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const generateId = () => {
+        return String(Math.floor(Math.random() * 10000000));
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons.concat(person);
+    res.json(person);
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id !== id);
+    res.status(204).end();
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'});
+}
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+
+    if(error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'});
+    }
+
+    next(error);
+}
+
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
